2251be982230dcc314a44bf02c408263eda07a9b
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-11 18:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo() {
+  return "Happy holidays, ${name}!"
+}
+
+function happyHolidaysTo() {
+  return "Happy ${holiday}, ${name}"
+}
+
+function holidayCountdown() {
+  return "It's ${days} days until ${holiday}!"
+}
